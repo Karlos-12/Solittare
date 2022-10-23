dbd26afa0ae00ac3b33dd5734109712c86a205be
Karel Cveček <kastrolekkaja@seznam.cz>
2022-10-24 00:35:42 +0200

    sdf

--- Solittare/Gamewindow.xaml.cs
+++ Solittare/Gamewindow.xaml.cs
@@ -38,11 +38,15 @@ namespace Solittare
                 {
                     string p1 = main.board[i - 1].cards[u - 1].color.ToString();
                     string p2 = main.board[i - 1].cards[u - 1].id.ToString();
-                    img.Source = new BitmapImage(new Uri("Resources/Cards/" + p1+"/"+p2+".png", UriKind.Relative));
-
+                    img.Source = new BitmapImage(new Uri("Resources/Cards/" + p1 + "/" + p2 + "n.png", UriKind.Relative));
+                    img.VerticalAlignment = VerticalAlignment.Top;
+                    img.HorizontalAlignment = HorizontalAlignment.Center;
                     
-                    Grid.SetColumn(img, u-1);
-                    img.Margin = new Thickness(0, nm * 15, 0, 0);
+
+                    Grid.SetColumn(img, i-1);
+                    Grid.SetRow(img, 0);
+                    img.Margin = new Thickness(5, nm * 15, 5, 0);
+                    nm++;
 
                     deskoc.Children.Add(img);
                 }
